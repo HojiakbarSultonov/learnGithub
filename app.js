cd1d3dd4be9d69ed151f517d0775017a5d2370c8
Hojiakbar <hojiakbarrasulovich@gmail.com>
2023-04-26 14:44:32 +0500

    learn Github

---
+++ app.js
@@ -0,0 +1,11 @@
+' use strict'
+
+// const place = confirm(' Are you here?')
+// console.log(place)
+
+// const currentPlace = prompt('where are you?', 'Hoji')
+
+const favouriteColor = []
+
+favouriteColor[0] = "Hoji"
+console.log(favouriteColor)
